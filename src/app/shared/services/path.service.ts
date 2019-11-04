3465365e585b5ccef5c7b5b4be68b9668a923ee8
Alberto Farci <tarcisio.albi@gmail.com>
2019-11-04 11:55:18 +0100

    added the comparization between paths

--- src/app/shared/services/path.service.ts
+++ src/app/shared/services/path.service.ts
@@ -10,8 +10,7 @@ export class PathService {
     constructor(private http: HttpClient) { }
     
     getPath(pointStart, pointEnd, filter) {
-        console.log(`http://156.148.14.188:8080/v1/requestTrip/31-12-2001/`+ pointStart + '/'+ pointEnd + '/'+filter.id  )
-        return this.http.get<any>(`http://156.148.14.188:8080/v1/requestTrip/31-12-2001/`+ pointStart + '/'+ pointEnd + '/'+ filter.id )
+        return this.http.get<any>(`http://156.148.14.188:8080/v1/requestTrip/31-12-2001/`+ pointStart + '/'+ pointEnd + '/'+ filter )
     }
 
     
