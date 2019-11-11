3d2ca8f4e39bca606543c25ad0bcbe51bc1d3356
Alberto Farci <tarcisio.albi@gmail.com>
2019-11-11 14:14:49 +0100

    added local notification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Map, tileLayer, marker, icon } from 'leaflet';
 import { Http } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { PathService } from '../shared/services/path.service';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-home',
@@ -20,13 +21,24 @@ export class HomePage {
 
   constructor(public http: Http,
             public plt: Platform,
-            public router: Router) {
+            public router: Router,
+            private localNotifications: LocalNotifications) {
 
             }
 
   setTypePAth(){
 
     this.router.navigate(["/tabs/filters"]);
+  }
+
+  single_notification() {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification',
+      data: { secret: 'key_data' },
+      smallIcon: 'assets://bb/icon3-m.png'
+    });
   }
 
   goToProfile(){
