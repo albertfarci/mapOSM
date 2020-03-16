3703057dd30e6e49d78e04e611acc7f19d3461fc
Alberto Farci <tarcisio.albi@gmail.com>
2020-03-16 10:09:17 +0100

    add back button press logic
    If the user is in the home page close the app
    If the useris in the search page bac to map page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public router: Router
   ) {
     this.initializeApp();
   }
@@ -23,5 +25,16 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
+
+    this.platform.backButton.subscribe(() => {
+      if (window.location.pathname == "/tabs/home") {
+        navigator['app'].exitApp();
+      }
+      if (window.location.pathname.includes("/tabs/search")) {
+        this.router.navigate(['/tabs/map']);
+      }
+
+    })
   }
 }
