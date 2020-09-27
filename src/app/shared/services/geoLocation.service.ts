61b364b5b9c6283b21dca281336ae8a020cc12d1
Alberto Farci <tarcisio.albi@gmail.com>
2020-09-27 19:16:38 +0200

    Upgrade parametri da inviare con API Segments

--- src/app/shared/services/geoLocation.service.ts
+++ src/app/shared/services/geoLocation.service.ts
@@ -200,7 +200,7 @@ export class GeoLocationService {
                 "mode": modalita,
                 "distance": 0,
                 "valid": 1,
-                "origin": 1,
+                "origin": 2,
                 "timestamps": timestamps
             }
             , { headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer 0e4d596a1d6ec5461fb7b48c1efe535702f6c69534a4c7e1ed96b02d08e1937cec211747b5d350bf5ae43a2dcfc11ab8870ee1bc4a3f3afaae37d34fb08548fb6fe3cc80b5c221c3f3d137df55a96318c985b97c6bbd1c225c4566700b4a443fe804536bea946db76eac189a397a281f46b1b6d688658229bbdd2e117879ae0a97e9c9946bb7d27d06e2460e5b82b27b0872e53f4bf00f49936232f2abf731e64f23a7fd87a1b3dc9f95f49bc7ad252098362973c5c38f195ea9e160871de593' } });
